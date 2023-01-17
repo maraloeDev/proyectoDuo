dc84ccc05355de53e8b9d3ea7278f88d1da9e5fd
maraloeDev <martinsonsecaeduardo@gmail.com>
2023-01-17 20:26:37 +0100

    ARCHIVO CREADO

---
+++ HolaDAM/src/HolaDAM.java
@@ -0,0 +1,17 @@
+/**
+ * ****************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ * ****************************************************************************
+ */
+
+/**
+ *
+ * @author marti
+ */
+public class HolaDAM {
+    
+    public static void main(String[] args) {
+        System.out.println("Esta es la 2da practica realozada por Eduardo Martin-Sonseca");
+    }
+
+}
